13ca2be5c6050727750dd852f0b3f73f97c9de50
glennhkm <glenn@mhs.usk.ac.id>
2024-09-28 20:08:30 +0700

    feat: add some components to tentang section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QuestionWrapper } from "@/components/questionWrapper/questionWrapper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { defaultGejala } from "@/data/defaultGejala";
 import { informasiGejala } from "@/data/informasiGejala";
 import toast from "react-hot-toast";
@@ -18,17 +18,23 @@ import { DiagnosaModal } from "@/components/modals/diagnosaModal";
 import { Spotlight } from "@/components/aceternity/spotlight";
 import Image from "next/image";
 // import Lottie from "lottie-react";
-// import Lungs from "@/../public/assets/lottie/lungs.json";
+import Lungs from "@/../public/assets/lottie/lungs.json";
 import { ArrowDownIcon } from "@radix-ui/react-icons";
 import { SparklesCore } from "@/components/aceternity/sparkles";
 import { Navbar } from "@/components/navbar/navbar";
 import { ScrollToSection } from "@/utils/helpers/scrollToSection";
+import { BackgroundGradient } from "@/components/aceternity/bg-gradient";
+import { AnimatedTooltip } from "@/components/aceternity/animated-tooltip";
+import { anggotaKelompok } from "@/data/anggotaKelompok";
+import Lottie from "lottie-react";
 
 const Main = () => {
   const [gejalaUser, setGejalaUser] = useState(defaultGejala);
   const [diagnosa, setDiagnosa] = useState<PenyakitDanSolusi[]>([]);
   const [diagnosaInfo, setDiagnosaInfo] = useState("");
   const [showDiagnosa, setShowDiagnosa] = useState(false);
+  const [isIntro, setIsIntro] = useState(true);
+  const [isSpotlight, setIsSpotlight] = useState(false);
 
   const handleYesButton = (gejala: string) => {
     setGejalaUser((prevGejalaUser) => ({
@@ -110,8 +116,17 @@ const Main = () => {
     }
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsIntro(false);
+    }, 3200);
+    setTimeout(() => {
+      setIsSpotlight(true);
+    }, 1800);
+  });
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       {showDiagnosa && (
         <DiagnosaModal
           diagnosa={diagnosa}
@@ -119,16 +134,46 @@ const Main = () => {
           setShowDiagnosa={setShowDiagnosa}
         />
       )}
-      <Navbar/>
+      {isIntro && (
+        <div
+          className="bg-[#151515] w-screen h-screen z-50 top-0 left-0 text-white flex justify-center items-center fixed"
+          style={{
+            animation: "slideDown 1s forwards",
+            animationDelay: "3s",
+          }}
+        >
+          <p className="text-[10rem] font-bold">KELOMPOK 6</p>
+          <div className="absolute top-0 left-0 w-screen z-50">
+            <Marquee
+              direction="left"
+              className="bg-white text-black text-xs p-2"
+            >
+              <p>
+                Program ini hanya berfungsi sebagai alat bantu diagnosa dan
+                telah dihitung memiliki akurasi sebesar 80%. Penting untuk
+                dipahami bahwa hasil yang diberikan bukanlah jaminan kebenaran
+                100%, dan masih ada kemungkinan adanya kondisi medis lain yang
+                tidak terdeteksi oleh program ini. Oleh karena itu, disarankan
+                untuk tetap berkonsultasi dengan tenaga medis profesional guna
+                mendapatkan diagnosis yang lebih akurat dan menyeluruh.
+              </p>
+              <p className="w-[50vw] h-full"></p>
+            </Marquee>
+          </div>
+        </div>
+      )}
+      <div id="navbar">
+        <Navbar />
+      </div>
       <div
         id="hero"
-        className="w-full h-screen to-transparent relative px-16 bg-black/20 bg-grid-white/10"
+        className="w-full h-screen to-transparent relative px-16 bg-grid-white/5"
       >
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <Spotlight />
+        {isSpotlight && <Spotlight />}
         <div className="w-full h-full flex items-center justify-center">
           <div className="w-1/2 flex flex-col gap-4 text-white/80 z-30 items-start">
-            <h1 className="text-7xl font-bold">Pulmo Health</h1>
+            <h1 className="text-7xl font-bold">PulmoHealth</h1>
             <div className="w-full relative">
               <div className="absolute inset-x-0 top-0 bg-gradient-to-r from-indigo-500 via-transparent to-transparent h-[2px] w-full blur-sm" />
               <div className="absolute inset-x-0 top-0 bg-gradient-to-r from-indigo-500 via-transparent to-transparent h-px w-full" />
@@ -144,19 +189,21 @@ const Main = () => {
               />
             </div>
             <p className="">
-              Sistem AI berbasis aturan yang membantu mendiagnosa penyakit
-              paru-paru Anda melalui analisis jawaban dari serangkaian
-              pertanyaan yang disesuaikan. Dapatkan evaluasi kesehatan paru-paru
-              yang lebih cepat dan akurat dengan PulmoHealth.
+              Sistem AI berbasis aturan yang mendiagnosis penyakit paru-paru
+              melalui analisis jawaban Anda. Dapatkan evaluasi paru-paru yang
+              cepat dan akurat dengan PulmoHealth.
             </p>
-            <button onClick={() => ScrollToSection("diagnosa")} className="w-48 py-2.5 rounded-xl border-[0.1px] border-black/20  shadow-balance shadow-sky-500 hover:shadow-slate-200 hover:scale-105 duration-150 mt-2 text-sm flex gap-2 items-center justify-center">
+            <button
+              onClick={() => ScrollToSection("diagnosa")}
+              className="w-48 py-2.5 rounded-xl border-[0.1px] border-black/20  shadow-balance shadow-sky-500 hover:shadow-slate-200 hover:scale-105 duration-150 mt-2 text-sm flex gap-2 items-center justify-center"
+            >
               <p>Mulai Sekarang</p>
               <ArrowDownIcon />
             </button>
           </div>
           <div className="w-[40%]">
             <Image
-              src="/assets/images/person-lungs-removebg.png"
+              src="/assets/images/person-lungs.webp"
               alt="Lungs"
               layout="responsive"
               width={500}
@@ -166,34 +213,63 @@ const Main = () => {
           </div>
         </div>
       </div>
-      <div className="fixed bottom-0 left-0 z-50">
-        <Marquee
-          direction="left"
-          className="bg-white text-black text-xs p-2"
-        >
-          <p>Program ini hanya berfungsi sebagai alat bantu diagnosa dan telah dihitung memiliki akurasi sebesar 80%. Penting untuk dipahami bahwa hasil yang diberikan bukanlah jaminan kebenaran 100%, dan masih ada kemungkinan adanya kondisi medis lain yang tidak terdeteksi oleh program ini. Oleh karena itu, disarankan untuk tetap berkonsultasi dengan tenaga medis profesional guna mendapatkan diagnosis yang lebih akurat dan menyeluruh.</p>
-          <p className="w-[50vw] h-full"></p>
-        </Marquee>
-      </div>
-      {/* <div
-        className="bg-[#151515] w-screen h-screen z-50 top-0 left-0 text-white flex justify-center items-center fixed"
-        style={{
-          animation: 'slideDown 1s forwards',
-          animationDelay: '2s'  
-        }}
-      >
-        <p className="text-[10rem] font-bold">KELOMPOK 6</p>
-        <div className="absolute top-0 left-0 w-screen z-50">
-          <Marquee
-            direction="left"
-            className="bg-white text-black text-xs p-2"
-          >
-            <p>Program ini hanya berfungsi sebagai alat bantu diagnosa dan telah dihitung memiliki akurasi sebesar 80%. Penting untuk dipahami bahwa hasil yang diberikan bukanlah jaminan kebenaran 100%, dan masih ada kemungkinan adanya kondisi medis lain yang tidak terdeteksi oleh program ini. Oleh karena itu, disarankan untuk tetap berkonsultasi dengan tenaga medis profesional guna mendapatkan diagnosis yang lebih akurat dan menyeluruh.</p>
-            <p className="w-[50vw] h-full"></p>
-          </Marquee>
+      <div id="tentang" className="w-full h-screen flex items-center relative px-16">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black/60 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="grid grid-cols-3 w-1/2 gap-2">          
+          <BackgroundGradient className="rounded-xl w-full p-4 sm:p-10 bg-zinc-900 relative h-[30vh] cursor-pointer group">
+            <div className="w-full h-full bg-black/60 absolute inset-0 z-30 flex justify-center items-center rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-150">
+              <p className="font-bold text-white text-xl">Jurnal Sumber</p>
+            </div>
+            <Image
+              src="/assets/images/document.jpg"
+              alt="document"
+              layout="fill"
+              className="object-cover w-full h-full rounded-xl"
+            />
+          </BackgroundGradient>          
+          <BackgroundGradient className="rounded-xl w-full justify-center items-center p-4 sm:p-10 bg-zinc-900 relative h-[30vh] cursor-pointer group">
+            <div className="w-full h-full bg-black/60 absolute inset-0 z-30 flex justify-center items-center rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-150">
+              <p className="font-bold text-white text-xl">Jurnal Sumber</p>
+            </div>
+            <Lottie animationData={Lungs} style={{
+              width: "120%",
+              height: "120%",
+            }}/>
+          </BackgroundGradient>          
+          <BackgroundGradient className="rounded-xl w-full p-4 sm:p-10 bg-zinc-900 relative h-[30vh] cursor-pointer group">
+            <div className="w-full h-full bg-black/60 absolute inset-0 z-30 flex justify-center items-center rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-150">
+              <p className="font-bold text-white text-xl">Jurnal Sumber</p>
+            </div>
+            <Image
+              src="/assets/images/document.jpg"
+              alt="document"
+              layout="fill"
+              className="object-cover w-full h-full rounded-xl"
+            />
+          </BackgroundGradient>          
+          <div className="w-full col-span-3">
+          <BackgroundGradient className="rounded-[22px] w-full p-4 sm:p-10 bg-zinc-900 col-span-3 ">
+            asndnjdn andnja nijdnq
+          </BackgroundGradient>
+          </div>
         </div>
-      </div> */}
-      <div id="diagnosa" className="w-full py-12 px-16 h-full z-20 bg-gradient-to-b from-black/90 to-transparent via-transparent">
+        <div className="flex flex-col gap-3 px-16 text-white/80 w-1/2 relative">
+          <div className="absolute top-0 right-10 flex">
+          <AnimatedTooltip items={anggotaKelompok} />
+          </div>
+          <h2 className="text-7xl font-bold">Tentang PulmoHealth</h2>
+          <p>
+            Sistem AI berbasis aturan yang membantu mendiagnosa penyakit
+            paru-paru Anda melalui analisis jawaban dari serangkaian pertanyaan
+            yang disesuaikan. Dapatkan evaluasi kesehatan paru-paru yang lebih
+            cepat dan akurat dengan PulmoHealth.
+          </p>
+        </div>
+      </div>
+      <div
+        id="diagnosa"
+        className="w-full py-12 px-16 h-full z-20 bg-gradient-to-b from-black/90 to-transparent via-transparent"
+      >
         <h1 className="font-bold text-8xl text-center text-white mt-8">
           PulmoHealth
         </h1>
@@ -221,6 +297,20 @@ const Main = () => {
             <ArrowIcon className="size-12" />
           </button>
         </div>
+      </div>
+      <div id="marquee" className="fixed bottom-0 left-0 z-40">
+        <Marquee direction="left" className="bg-white text-black text-xs p-2">
+          <p>
+            Program ini hanya berfungsi sebagai alat bantu diagnosa dan telah
+            dihitung memiliki akurasi sebesar 80%. Penting untuk dipahami bahwa
+            hasil yang diberikan bukanlah jaminan kebenaran 100%, dan masih ada
+            kemungkinan adanya kondisi medis lain yang tidak terdeteksi oleh
+            program ini. Oleh karena itu, disarankan untuk tetap berkonsultasi
+            dengan tenaga medis profesional guna mendapatkan diagnosis yang
+            lebih akurat dan menyeluruh.
+          </p>
+          <p className="w-[50vw] h-full"></p>
+        </Marquee>
       </div>
     </div>
   );
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,14 +9,10 @@ export const Navbar: React.FC = () => {
   const controlNavbar = () => {
     if (typeof window !== "undefined") {
       if (window.scrollY > lastScrollY) {
-        // if scroll down hide the navbar
         setIsVisible(false);
       } else {
-        // if scroll up show the navbar
         setIsVisible(true);
       }
-
-      // remember current page location to use in the next move
       setLastScrollY(window.scrollY);
     }
   };
@@ -34,18 +30,18 @@ export const Navbar: React.FC = () => {
 
   return (
     <div
-      className={`bg-[#151515] shadow-balance shadow-slate-400 fixed top-5 left-1/2 -translate-x-1/2 pr-4 rounded-3xl flex gap-10 text-sm z-[50] text-white justify-center transition-all duration-300 ${
+      className={`bg-[#151515] shadow-balance shadow-slate-400 fixed top-8 left-1/2 -translate-x-1/2 pr-4 rounded-3xl flex gap-10 text-sm z-40 text-white justify-center transition-all duration-300 ${
         isVisible ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
       }`}
     >
-      <div className="flex items-center cursor-pointer" onClick={() => ScrollToSection("hero")}>
+      <div className="flex items-center cursor-pointer hover:scale-105 duration-150" onClick={() => ScrollToSection("hero")}>
         <div className="w-10 h-10 relative">
           <Image src="/assets/images/logo.png" alt="logo" fill />
         </div>
         <p className="font-bold">PulmoHealth</p>
       </div>
-      <button onClick={() => ScrollToSection("tentang")}>Tentang</button>
-      <button onClick={() => ScrollToSection("diagnosa")}>Cek Diagnosa</button>
+      <button className="hover:scale-110 duration-150" onClick={() => ScrollToSection("tentang")}>Tentang</button>
+      <button className="hover:scale-110 duration-150" onClick={() => ScrollToSection("diagnosa")}>Cek Diagnosa</button>
     </div>
   );
 };
